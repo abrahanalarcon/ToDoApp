86f663b6c1856dfdb2fc6f169cd0fcdf4d024350
abrahanalarcon <moiseshazim@gmail.com>
2025-06-30 20:25:38 -0400

    creando clase y servicio para conexion la db

---
+++ Models/Tarea.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class Tarea
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public bool Completada { get; set; }
+    }
+}
---
+++ Services/DatabaseService.cs
@@ -0,0 +1,44 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.Services
+{
+    public class DatabaseService
+    {
+        private SQLiteAsyncConnection _db;
+
+        public async Task Init()
+        {
+            if (_db is not null)
+                return;
+
+            var ruta = Path.Combine(FileSystem.AppDataDirectory, "Tareas.db");
+            _db = new SQLiteAsyncConnection(ruta);
+            await _db.CreateTableAsync<Tarea>();
+        }
+
+        public async Task<List<Tarea>> GetTareasAsync()
+        {
+            await Init();
+            return await _db.Table<Tarea>().ToListAsync();
+        }
+
+        public async Task AddTareaAsync(Tarea tarea)
+        {
+            await Init();
+            await _db.InsertAsync(tarea);
+        }
+
+        public async Task DeleteTareaAsync(Tarea tarea)
+        {
+            await Init();
+            await _db.DeleteAsync(tarea);
+        }
+
+    }
+}
